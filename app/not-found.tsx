8764022bf1d485db5feec9b31869ebd715523f86
g-120902 <gregoryalbert1209@gmail.com>
2023-12-30 01:30:55 +0400

    solving issue with layout 2

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,5 +2,5 @@ import {useTranslations} from 'next-intl';
  
 export default function NotFoundPage() {
   const t = useTranslations('NotFound');
-  return <h1>{t('title')}</h1>;
+  return <h1>Not Found</h1>;
 }
