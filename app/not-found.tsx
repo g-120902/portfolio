348afe2deba1af046b87c7fb2de0972c31b8d667
g-120902 <gregoryalbert1209@gmail.com>
2023-12-30 01:55:07 +0400

    correcting page not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,3 @@
-import { useTranslations } from "next-intl";
-
 export default function NotFoundPage() {
-  const t = useTranslations('NotFound');
-  return <h1>{t('title')}</h1>;
+  return <h1>Not Found</h1>;
 }
