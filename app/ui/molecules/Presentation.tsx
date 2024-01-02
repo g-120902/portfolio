6ebe86432b246891645e9d34c2d1f684fe07f32b
g-120902 <gregoryalbert1209@gmail.com>
2024-01-02 14:39:32 +0400

    ajustment in font

--- app/ui/molecules/Presentation.tsx
+++ app/ui/molecules/Presentation.tsx
@@ -1,6 +1,7 @@
 import Button from "../atoms/Button";
 import {useTranslations} from 'next-intl';
 import Avatar from "../atoms/Avatar";
+import { playfair } from "../../lib/fonts";
 
 function Presentation(){
     const t = useTranslations('Presentation');
@@ -11,7 +12,7 @@ function Presentation(){
                 <Avatar />
 
                         <div className="flex flex-col gap-5 pt-4 md:pt-8 md:pb-12 pb-8">
-                            <h1 className="font-bold text-white lg:text-5xl text-3xl md:text-4xl">Gregory Albert</h1>
+                            <h1 className={`${playfair.className} font-bold text-white lg:text-5xl text-3xl md:text-4xl`}>Gregory Albert</h1>
                             <p className="text-black text-sm font-extralight">{t("desc")}</p>
                         </div>
                         <Button text={t("hire")} />
