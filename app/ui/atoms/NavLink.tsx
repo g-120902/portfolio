622e4d0e147122288762865a8cc321ba5f102f83
g-120902 <gregoryalbert1209@gmail.com>
2024-02-09 08:26:40 +0400

    correcting language in navbar

--- app/ui/atoms/NavLink.tsx
+++ app/ui/atoms/NavLink.tsx
@@ -2,12 +2,14 @@
 
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { getRedirectPath } from "../../utils/getRedirect";
+import { getLocalePath, getRedirectPath } from "../../utils/getRedirect";
 import { Link } from "../../lib/navigation";
+import { Locale } from "../../types/enum";
 
 function NavLink({ link, text }: { link: string, text: string }) {
 
     const pathName = usePathname();
+    const locale: Locale = getLocalePath(pathName) as Locale;
     const [redirect, setRedirect] = useState(getRedirectPath(pathName));
 
     useEffect(() => {
@@ -15,7 +17,7 @@ function NavLink({ link, text }: { link: string, text: string }) {
     }, [pathName]);
 
     return (
-        <Link href={link} className={redirect.substring(1) != link ? "hover:text-skin-hover hover:underline self-center" : "hover:text-skin-hover underline self-center "}>
+        <Link href={link} locale={locale} className={redirect.substring(1) != link ? "hover:text-skin-hover hover:underline self-center" : "hover:text-skin-hover underline self-center "}>
             {text}
         </Link>
     )
