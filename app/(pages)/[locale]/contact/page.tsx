f8f84f27def8765eae701cb51b9f9ea48199ea19
g-120902 <gregoryalbert1209@gmail.com>
2024-02-09 15:19:13 +0400

    deployment 2

--- app/(pages)/[locale]/contact/page.tsx
+++ app/(pages)/[locale]/contact/page.tsx
@@ -1,11 +1,4 @@
 import ContactBody from "../../../ui/organisms/ContactBody";
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Gregory Albert Portfolio Contact Page',
-  description: 'This is Gregory Albert portfolio. The contact page of my portfolio used for mailing',
-  
-}
 
 export default function Contact() {
   return (
